f58cd1798853f31a7f451518588dc48b7922baf8
mohittushirgit <mohittushir099@gmail.com>
2024-12-03 17:18:14 +0530

    "Removed projectlinkby.js file and its script tag from index.html"

---
+++ projectlinkby.js
@@ -0,0 +1,5 @@
+let projectLink = document.querySelector('#firstcard');
+projectLink.addEventListener('click',()=>{
+   window.location.href='file:///C:/Users/mtush/OneDrive/Desktop/Portfolio/Projects/Todo-list/todolist.html';
+
+});
