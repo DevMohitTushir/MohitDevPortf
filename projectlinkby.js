174ed954c39748b002eaf8c5cb0281755975645d
mohittushirgit <mohittushir099@gmail.com>
2024-12-03 17:19:14 +0530

    new

--- projectlinkby.js
+++
@@ -1,5 +0,0 @@
-let projectLink = document.querySelector('#firstcard');
-projectLink.addEventListener('click',()=>{
-   window.location.href='file:///C:/Users/mtush/OneDrive/Desktop/Portfolio/Projects/Todo-list/todolist.html';
-
-});
